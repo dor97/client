97fd1fa29918544b3e6d2edaf082542d4fe97eb6
Dor <dorraba100@gmail.com>
2023-10-15 11:34:06 +0300

    new commit

--- src/httpClient/clientCommunication.java
+++ src/httpClient/clientCommunication.java
@@ -23,7 +23,7 @@ import static httpClient.Configuration.HTTP_CLIENT;
 
 public class clientCommunication {
 
-    private String BASE_URL = "http://localhost:8080/Server_Web_exploded";
+    private String BASE_URL = "http://localhost:8080/Server_Web";
     private String userName = "user";
     private Boolean isAdmin = false;
     private String chosenSimulationName;
